e7d9367c6016045eccf4735bfaf3eac90f681f74
Shravani Bhoir <shravanibhoir1408@gmail.com>
2026-06-22 07:27:09 +0000

    added the data

---
+++ database/sample_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO locations
+(area_name, city, latitude, longitude)
+VALUES
+('Andheri', 'Mumbai', 19.1197, 72.8468),
+('Bandra', 'Mumbai', 19.0596, 72.8295),
+('Dadar', 'Mumbai', 19.0184, 72.8432),
+('Kurla', 'Mumbai', 19.0728, 72.8826),
+('Thane', 'Mumbai', 19.2183, 72.9781),
+('Navi Mumbai', 'Mumbai', 19.0330, 73.0297);
